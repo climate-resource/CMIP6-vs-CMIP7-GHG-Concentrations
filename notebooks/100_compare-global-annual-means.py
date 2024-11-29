484d77fa7da40eab043e19199c496edb7848d6dd
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-11-29 10:57:09 +0100

    Add ERF difference

--- notebooks/100_compare-global-annual-means.py
+++ notebooks/100_compare-global-annual-means.py
@@ -25,13 +25,19 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import openscm_units
 import pandas as pd
+import pint
 import seaborn as sns
 import tqdm
 import xarray as xr
 from input4mips_validation.cvs.drs import DataReferenceSyntax
 
 from utils import PROCESSED_DATA_DIR, RAW_DATA_DIR
+
+# %%
+pint.set_application_registry(openscm_units.unit_registry)
+Q = pint.get_application_registry().Quantity
 
 # %% [markdown]
 # # Load CMIP data
@@ -248,9 +254,10 @@ def load_cmip7_data(fps: list[Path]) -> xr.Dataset:
 
 # %%
 to_load = db[db["variable_normalised"].isin([
-    # "co2", 
-    # "ch4", 
-    # "n2o",
+    "co2", 
+    "ch4", 
+    "n2o",
+    
     # # WMO 2022 Ch. 7 variables start
     "cfc11",
     "cfc12",
@@ -271,18 +278,19 @@ to_load = db[db["variable_normalised"].isin([
     "hcfc22",
     # # Western variables end
     # WMO 2022 Ch. 7 variables end
-    # # # Velders et al., 2022 variables start
-    # "hfc32",
-    # "hfc125",
-    # "hfc134a",
-    # "hfc143a",
-    # "hfc152a",
-    # "hfc227ea",
-    # "hfc236fa",
-    # "hfc245fa",
-    # "hfc365mfc",
-    # "hfc4310mee",
-    # # # Velders et al., 2022 variables end
+    
+    # # Velders et al., 2022 variables start
+    "hfc32",
+    "hfc125",
+    "hfc134a",
+    "hfc143a",
+    "hfc152a",
+    "hfc227ea",
+    "hfc236fa",
+    "hfc245fa",
+    "hfc365mfc",
+    "hfc4310mee",
+    # # Velders et al., 2022 variables end
 
     # Equivalent species start
     "cfc11eq",
@@ -290,22 +298,22 @@ to_load = db[db["variable_normalised"].isin([
     "hfc134aeq",
     # Equivalent species end
     
-    # # Other
-    # "hfc23",
-    # "cf4",
-    # "c2f6",
-    # "c3f8",
-    # "c4f10",
-    # "c5f12",
-    # "c6f14",
-    # "c7f16",
-    # "c8f18",
-    # "cc4f8",
-    # "ch2cl2",
-    # "chcl3",
-    # "nf3",
-    # "sf6",
-    # "so2f2",
+    # Other
+    "hfc23",
+    "cf4",
+    "c2f6",
+    "c3f8",
+    "c4f10",
+    "c5f12",
+    "c6f14",
+    "c7f16",
+    "c8f18",
+    "cc4f8",
+    "ch2cl2",
+    "chcl3",
+    "nf3",
+    "sf6",
+    "so2f2",
 ])]
 
 # %%
@@ -502,7 +510,8 @@ for time_range in (
             y=data_var,
             ax=axes[data_var],
             # linewidth=2, 
-            alpha=0.7
+            alpha=0.4,
+            s=75,
         )
 
         axes[data_var].axhline(0, linestyle="--", color="k")
@@ -519,7 +528,76 @@ print("done")
 # ### Changes since CMIP6
 
 # %%
-for data_var in sorted(loaded.data_vars):
+# Table 7.SM.6 of https://www.ipcc.ch/report/ar6/wg1/downloads/report/IPCC_AR6_WGI_Chapter07_SM.pdf
+RADIATIVE_EFFICIENCIES: dict[str, pint.UnitRegistry.Quantity] = {
+    "co2": Q(1.33e-5, "W / m^2 / ppb"),
+    "ch4": Q(0.000388, "W / m^2 / ppb"),
+    "n2o": Q(0.0032, "W / m^2 / ppb"),
+    # Chlorofluorocarbons
+    "cfc11": Q(0.291, "W / m^2 / ppb"),
+    "cfc11eq": Q(0.291, "W / m^2 / ppb"),
+    "cfc12": Q(0.358, "W / m^2 / ppb"),
+    "cfc12eq": Q(0.358, "W / m^2 / ppb"),
+    "cfc113": Q(0.301, "W / m^2 / ppb"),
+    "cfc114": Q(0.314, "W / m^2 / ppb"),
+    "cfc115": Q(0.246, "W / m^2 / ppb"),
+    # Hydrofluorochlorocarbons
+    "hcfc22": Q(0.214, "W / m^2 / ppb"),
+    "hcfc141b": Q(0.161, "W / m^2 / ppb"),
+    "hcfc142b": Q(0.193, "W / m^2 / ppb"),
+    # Hydrofluorocarbons
+    "hfc23": Q(0.191, "W / m^2 / ppb"),
+    "hfc32": Q(0.111, "W / m^2 / ppb"),
+    "hfc125": Q(0.234, "W / m^2 / ppb"),
+    "hfc134a": Q(0.167, "W / m^2 / ppb"),
+    "hfc134aeq": Q(0.167, "W / m^2 / ppb"),
+    "hfc143a": Q(0.168, "W / m^2 / ppb"),
+    "hfc152a": Q(0.102, "W / m^2 / ppb"),
+    "hfc227ea": Q(0.273, "W / m^2 / ppb"),
+    "hfc236fa": Q(0.251, "W / m^2 / ppb"),
+    "hfc245fa": Q(0.245, "W / m^2 / ppb"),
+    "hfc365mfc": Q(0.228, "W / m^2 / ppb"),
+    "hfc4310mee": Q(0.357, "W / m^2 / ppb"),
+    # Chlorocarbons and Hydrochlorocarbons
+    "ch3ccl3": Q(0.065, "W / m^2 / ppb"),
+    "ccl4": Q(0.166, "W / m^2 / ppb"),
+    "ch3cl": Q(0.005, "W / m^2 / ppb"),
+    "ch2cl2": Q(0.029, "W / m^2 / ppb"),
+    "chcl3": Q(0.074, "W / m^2 / ppb"),
+    # Bromocarbons, Hydrobromocarbons and Halons
+    "ch3br": Q(0.004, "W / m^2 / ppb"),
+    "halon1211": Q(0.300, "W / m^2 / ppb"),
+    "halon1301": Q(0.299, "W / m^2 / ppb"),
+    "halon2402": Q(0.312, "W / m^2 / ppb"),
+    # Fully Fluorinated Species
+    "nf3": Q(0.204, "W / m^2 / ppb"),
+    "sf6": Q(0.567, "W / m^2 / ppb"),
+    "so2f2": Q(0.211, "W / m^2 / ppb"),
+    "cf4": Q(0.099, "W / m^2 / ppb"),
+    "c2f6": Q(0.261, "W / m^2 / ppb"),
+    "c3f8": Q(0.270, "W / m^2 / ppb"),
+    "cc4f8": Q(0.314, "W / m^2 / ppb"),
+    "c4f10": Q(0.369, "W / m^2 / ppb"),
+    "c5f12": Q(0.408, "W / m^2 / ppb"),
+    "c6f14": Q(0.449, "W / m^2 / ppb"),
+    "c7f16": Q(0.503, "W / m^2 / ppb"),
+    "c8f18": Q(0.558, "W / m^2 / ppb"),    
+}
+
+rad_eff_match_units = {}
+for k, v in RADIATIVE_EFFICIENCIES.items():
+    if k in ["co2"]:
+        rad_eff_match_units[k] = v.to("W / m^2 / ppm")
+    elif k in ["ch4", "n2o"]:
+        rad_eff_match_units[k] = v.to("W / m^2 / ppb")
+    else:
+        rad_eff_match_units[k] = v.to("W / m^2 / ppt")
+        
+rad_eff_match_units
+
+# %%
+# for data_var in sorted(loaded.data_vars):
+for data_var in ["co2", "ch4", "n2o", "cfc11eq", "cfc12eq", "hfc134aeq", "cfc11", "cfc12", "hfc134a"]:
     if "bnds" in data_var:
         continue
 
@@ -539,10 +617,22 @@ for data_var in sorted(loaded.data_vars):
     plt.tight_layout()
     plt.show()
 
+    difference = parray.sel(source_id=CMIP7_COMPARE_SOURCE_ID) - parray.sel(source_id=CMIP6_SOURCE_ID)
+
     try:
-        difference = parray.sel(source_id=CMIP7_COMPARE_SOURCE_ID) - parray.sel(source_id=CMIP6_SOURCE_ID)
+        difference_rf = difference * rad_eff_match_units[difference.name]
+            
+        fig, axes = plt.subplot_mosaic([["recent", "recent"], ["all", "historical"]], figsize=(10, 6))
+    
+        for time_axis, ax in ((slice(None, None), "all"), (slice(1950, None), "recent"), (slice(1750, None), "historical")):
+            difference_rf.sel(year=time_axis).plot.line(x="year", hue="source_id", alpha=0.9, ax=axes[ax])
+    
+        fig.suptitle(f"{parray.name} ERF difference ({CMIP7_COMPARE_SOURCE_ID} - {CMIP6_SOURCE_ID})")
+        
+        plt.tight_layout()
+        plt.show()
     except KeyError:
-        continue
+        print(f"No radiative efficiency for {data_var}")
         
     fig, axes = plt.subplot_mosaic([["recent", "recent"], ["all", "historical"]], figsize=(10, 6))
 
@@ -564,3 +654,7 @@ for data_var in sorted(loaded.data_vars):
     
     plt.tight_layout()
     plt.show()
+
+    # break
+
+# %%
